0ab957324ae0f5de75caab5730bfaeeb2d20bd6a
Jeffrey Miller <Millerjl07@mounties.mansfield.edu>
2018-04-09 10:55:30 -0400

    Added .bat file for installing new flask modules
    QuickPipUpdate contains the pip installs for flask modules. If adding
    more modules to the project, add them to the file and run it. Python
    will skip over already installed modules.

---
+++ OnePortal_Website/QuickPipUpdate.bat
@@ -0,0 +1,10 @@
+@echo off
+call portalvenv\Scripts\activate.bat
+
+pip install flasky
+pip install flask-wtf
+pip install flask-sqlalchemy
+pip install flask-migrate
+pip install flask-login
+
+:END
